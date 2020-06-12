10be82213e41001c5d22db105bb3c593188852c1
Juliana Assalti <jaf.juliana@gmail.com>
2020-06-12 20:46:35 -0300

    select option to api

--- src/components/BoxSearch/index.js
+++ src/components/BoxSearch/index.js
@@ -7,10 +7,38 @@ import { Box, ListType, Header, Content } from "./styles";
 import { IoIosCar } from "react-icons/io";
 import { MdMotorcycle, MdChevronRight } from "react-icons/md";
 
+import Vehicles from "../../services/Vehicles";
+
 function BoxSearch({ makes }) {
   const [activeTab, setActiveTab] = useState("0");
+  const [listModels, setListModels] = useState([]);
+  const [listVersions, setListVersions] = useState([]);
 
   const isActive = (id) => activeTab.includes(id);
+
+  async function handleMakes(e) {
+    if (e.target.value !== "Todos") {
+      const resp = await Vehicles.getModels(e.target.value);
+
+      if (resp.status === 200) {
+        setListModels(resp.data);
+      }
+    } else {
+      setListModels([]);
+    }
+  }
+
+  async function handleModel(e) {
+    if (e.target.value !== "Todos") {
+      const resp = await Vehicles.getVersions(e.target.value);
+
+      if (resp.status === 200) {
+        setListVersions(resp.data);
+      }
+    } else {
+      setListVersions([]);
+    }
+  }
 
   return (
     <Box>
@@ -67,12 +95,45 @@ function BoxSearch({ makes }) {
           </div>
 
           <div className="line">
-            <label for="makes">Marca:</label>
-
-            <select name="makes" id="makes">
+            <label htmlFor="makes">Marca:</label>
+            <select name="makes" id="makes" onChange={handleMakes}>
+              <option>Todos</option>
               {makes.map((make) => (
-                <option value={make.ID}>{make.Name}</option>
+                <option key={make.ID} value={make.ID}>
+                  {make.Name}
+                </option>
               ))}
+            </select>
+
+            <label htmlFor="models">Modelos:</label>
+            <select
+              name="models"
+              id="models"
+              onChange={handleModel}
+              disabled={listModels.length === 0}
+            >
+              <option>Todos</option>
+              {listModels.length > 0 &&
+                listModels.map((model) => (
+                  <option key={model.ID} value={model.ID}>
+                    {model.Name}
+                  </option>
+                ))}
+            </select>
+
+            <label htmlFor="versions">Versões:</label>
+            <select
+              name="versions"
+              id="versions"
+              disabled={listVersions.length === 0}
+            >
+              <option>Todos</option>
+              {listVersions.length > 0 &&
+                listVersions.map((version) => (
+                  <option key={version.ID} value={version.ID}>
+                    {version.Name}
+                  </option>
+                ))}
             </select>
           </div>
 
